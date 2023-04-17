44a174c7517eb1492171cc6e689d0e696615c3b2
sheikh md sarowar islam <sarwarislam@gmail.com>
2023-04-17 01:38:58 -0400

    update

---
+++ destructure.js
@@ -0,0 +1,12 @@
+ const ami = {name:'sarowar islam',age:27, job: 'nothing',gfName:'moyuri',phoneNumber:01991386659,adress: 'khill khet'};
+
+ const {name,age,job,salaey} = ami;
+//  const gf = ami.gfName;
+//  const phoneNumber1 = ami.phoneNumber;
+//  const ages = ami.age;
+
+//  console.log(gf,phoneNumber1,ages);
+//  console.log(gf,phoneNumber1,ages);
+//  console.log(gf,phoneNumber1,ages);
+//  console.log(gf,phoneNumber1,ages);
+console.log({name,age,job,salaey});
